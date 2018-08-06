3cc8942eadafbf1dd988584dd7bc05c93b82961d
Samed Karakus <karakusnavy@gmail.com>
2018-08-06 15:39:18 +0300

    first-update

---
+++ RepeaterApplication/RepeaterApplication/index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="RepeaterApplication.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+<asp:Repeater ID="rptAdi" OnItemCommand="rptAdi_ItemCommand" runat="server">
+    <ItemTemplate>
+        <asp:Label runat="server" ID="text"><%#Eval("name") %></asp:Label><!-- "name" is database column name (MYSQL) -->
+    </ItemTemplate>
+    </asp:Repeater>
+    </form>
+</body>
+</html>
---
+++ RepeaterApplication/RepeaterApplication/index.aspx.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RepeaterApplication
+{
+    public partial class index : System.Web.UI.Page
+    {
+
+        //edit this
+        public MySqlConnection con = new MySqlConnection("Server=localhost;Database=name;Uid=username;Pwd=password;Encrypt=false;AllowUserVariables=True;UseCompression=True;Charset=utf8");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MySqlCommand kmt = new MySqlCommand("Select * from tablename", con);
+            con.Open();
+            kmt.ExecuteNonQuery();
+            MySqlDataReader drem = kmt.ExecuteReader();
+            rptAdi.DataSource = drem;
+            rptAdi.DataBind();
+            drem.Close();
+            con.Close();
+        }
+    }
+}
